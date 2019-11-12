cf0754da37f8ec3e2590e69c51af60bae48aa446
trimakus <aymen.trimech@trimakus.com>
2019-11-12 17:35:23 +0100

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default
    axios.create({
        //backend URL
-     baseURL: 'http://127.0.0.1:8000/api/',
+     //baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'http://partez-bes.trimakus.com/api/',
       timeout: 60000
    });
