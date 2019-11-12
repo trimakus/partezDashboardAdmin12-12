50463bd70833ec6d681bba316fc91a5b49cdbb02
trimakus <aymen.trimech@trimakus.com>
2019-11-12 17:34:29 +0100

    Add files via upload

---
+++ src/constants/OauthClient.js
@@ -0,0 +1,7 @@
+
+const OauthClient = {
+    
+    oauthClient:'Y436S7NhzaJ9KRP2p6Q49O36dkorXqcpSmjaHw3K',
+    client_id:'2'
+}
+export default OauthClient;
---
+++ src/constants/UrlPhoto.js
@@ -0,0 +1,6 @@
+
+const UrlPhoto = {
+    urlphoto:'http://127.0.0.1:8000/photo/'
+
+}
+export default UrlPhoto;
