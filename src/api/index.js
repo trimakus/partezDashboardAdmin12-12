8c203b955f709ac1bd1eade7e62e966af0a1ef65
trimakus <aymen.trimech@trimakus.com>
2019-11-12 17:17:04 +0100

    test7

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default
-   axios.create({
-     //baseURL: 'http://127.0.0.1:8000/api/',
-       baseURL: 'http://partez-bes.trimakus.com/api/',
-       timeout: 60000
-   });
