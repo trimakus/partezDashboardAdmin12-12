18461eb3fe6b5e7972c2f398d5ba25576168e515
trimakus <aymen.trimech@trimakus.com>
2019-11-12 17:12:04 +0100

    test6

---
+++ src/constants/UrlPhoto.js
@@ -0,0 +1,10 @@
+
+const UrlPhoto = {
+//
+    urlphoto:'http://partez-bes.trimakus.com/photo/'
+    //urlphoto:'http://127.0.0.1:8000/photo/'
+
+
+}
+export default UrlPhoto;
+// urlphoto:'http://127.0.0.1:8000/photo/'
